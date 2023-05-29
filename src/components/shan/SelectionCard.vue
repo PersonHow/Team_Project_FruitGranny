96c44c359c48da9a4c235d3088e3d1626bc7ca0f
peishan-Chiang <peishanchiang0816@gmail.com>
2023-05-29 14:49:44 +0800

    0529commit

---
+++ src/components/shan/SelectionCard.vue
@@ -0,0 +1,224 @@
+
+<script>
+
+
+export default {
+    components: {
+
+
+    },
+    data() {
+        return {
+            selectionNum: null
+        }
+    }, props: [
+        "selectShopppingCode",
+        "selectId",
+        "selectName",
+        "selectNumber",
+        "selectPrice",
+        "selectStock",
+
+    ],
+
+    methods: {
+        intilization() {
+
+
+            this.selectionNum = this.selectNumber;
+            console.log(this.selectNumber)
+            console.log(this.selectionNum)
+        },
+        updateData() { //modify
+            let body = {
+                "shoppingCode": this.selectShopppingCode,
+                "product": this.selectId,
+                "number": this.selectionNum,
+                "buyerAccount": localStorage.getItem('email')
+            }
+            fetch("http://localhost:8080/modi_data", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            })
+                .then(response => {
+                    //vue不能用fetch+function 要用箭頭
+                    //從JSON格式轉回Js物件
+                    return response.json()
+                })
+                .then(data => {
+
+
+                    console.log(data);
+
+                    alert(data.msg);
+                    this.emitPushSelect();
+
+                })
+
+
+        },
+        deleteDate() {
+
+            let body = {
+                "shoppingCode":this.selectShopppingCode,
+                "buyerAccount":localStorage.getItem('email')
+                // "buyerAccount": "A124" //localstorage
+            }
+            fetch("http://localhost:8080/delete_data", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            })
+                .then(response => {
+                    //vue不能用fetch+function 要用箭頭
+                    //從JSON格式轉回Js物件
+                    return response.json()
+                })
+                .then(data => {
+
+                    console.log(data);
+
+                    alert(data.msg);
+                    this.emitPushSelect();
+
+                })
+
+        },
+        emitPushSelect() {
+            console.log(`CCC`)
+            this.$emit("emitPushModi")
+        }
+
+    },
+
+
+    // 生命週期}
+    mounted() {
+        // this.fn();
+        // 從 Localstorage 取得值並代入到 帳號
+        // this.selectionNum = this.selectNumber;
+        // this.selectionStock = this.selectStock;
+        this.intilization();
+    }
+}
+
+
+
+
+</script>
+<template>
+    <div class="card">
+        <div class="explainAndBtn">
+
+
+            <!-- <img src="../../../public/shan/img/sweet-potato-600x400.jpg" alt=""> -->
+            <div class="explain">
+
+
+                <div class="changeNumandAdd">
+
+                    <div class="content-arae">
+                        <div class="nameItem">
+                            <p>名稱：</p>
+                            <p class="text-danger">{{ selectName }} </p>
+                        </div>
+                        <p>價格： {{ selectPrice }} </p>
+                    </div>
+
+                    <div class="content-arae">
+                        <div class="nameItem">
+                            <p>庫存數量：</p>
+                            <p class="text-danger"> {{ selectStock }} </p>
+                        </div>
+                        <p>已選數量： {{ selectNumber }} </p>
+                    </div>
+                </div>
+
+                <div class="content-arae">
+                    <label for="addNum">數量異動</label>
+                    <input class="addNum" type="number" v-model="this.selectionNum">
+                    <p>項目價格： {{ selectPrice * selectNumber }} </p>
+                </div>
+
+                <div class="content-arae">
+                    <button type="button" class="addCartBTN" @click="updateData()">
+                        修改數量
+                    </button>
+                    <button type="button" class="addCartBTN" @click="deleteDate()">
+                        刪除項目
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.explainAndBtn {
+    overflow-y: auto;
+}
+
+.explain {
+    border-radius: 5px;
+    border: 2px solid #454141;
+    display: flex;
+    justify-content: space-evenly;
+
+}
+
+.changeNumandAdd {
+
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+}
+
+input {
+    text-align: center;
+}
+
+.nameItem {
+    display: flex;
+}
+
+.content-arae {
+    text-align: center;
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 10px;
+}
+
+.addNum {
+    width: 5vw;
+}
+
+.addCartBTN {
+    border-radius: 5px;
+    background: #454141;
+    color: antiquewhite;
+    transition: 1s;
+    width: 6vw;
+    margin-bottom: 3px;
+}
+
+.addCartBTN:hover {
+    background: antiquewhite;
+    color: #454141;
+    scale: 1.1;
+}
+
+.addCartBTN:active {
+    scale: 0.9;
+}
+</style>
--- src/components/shan/layout.vue
+++ src/components/shan/layout.vue
@@ -1,25 +1,40 @@
 <script>
-
-import layoutCardView from './layoutCard.vue'
+import selectionView from './SelectionCard.vue'
+import layoutCardView from './layoutCard.vue';
 export default {
+
     components: {
-        layoutCardView
+        layoutCardView,
+        selectionView
+
     },
     data() {
         return {
             searchResultArr: [],
-            searchData:null
-            // emitIndex: null,
-            // getIndex:null
-         
+            chartResultArr: [],
+            searchData: null,
+            searchTypeBtn: "搜尋分類",
+            searchTypePlace: "產地搜尋",
+            searchTypeProduct: "產品搜尋",
+            cityValue: null,
+            startDate: '', // 起始日期
+            endDate: '', // 結束日期
+            isStartDateInvalid: false, // 是否為無效的起始日期
+            totalPrice: null
+
+
+
+
+
 
 
         }
     },
-    methods: {
-        getProductInfo() {
-           
 
+
+    methods: {
+
+        getProductInfo() {
             let body = { //產品名稱
                 "name": this.searchData
             }
@@ -37,26 +52,186 @@ export default {
                     return response.json()
                 })
                 .then(data => {
-
-
                     console.log(data);
                     this.searchResultArr = data.product_list;
-                    
-                    console.log(this.searchResultArr);
+
+                    // console.log(this.searchResultArr);
 
 
                 })
         },
-     
+        getPlaceProductInfo() {
+            const tool = document.querySelector("#tool");
+            let getCityName = tool.value;
+            console.log(getCityName);
+
+            let body = { //產品名稱
+                "place": getCityName
+            }
+
+            fetch("http://localhost:8080/search_product", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            })
+                .then(response => {
+                    //vue不能用fetch+function 要用箭頭
+                    //從JSON格式轉回Js物件
+                    return response.json()
+                })
+                .then(data => {
+
+
+
+                    console.log(data);
+                    this.searchResultArr = data.searchAllRes;
+
+
+
+
+                })
+        },
+        changeAndSearchPlace() { //產地搜尋
+            this.searchTypeBtn = this.searchTypePlace;
+            this.getPlaceProductInfo();
+        },
+        changeAndSearchNmae() { //產品搜尋
+            this.searchTypeBtn = this.searchTypeProduct;
+            this.getProductInfo();
+        },
+        getDateInfo() {
+            console.log(this.startDate);
+            console.log(this.endDate);
+            if (this.startDate > this.endDate || this.isStartDateInvalid === true) {
+                alert('Warning!!! 你提供的日期不符合規定')
+            }
+
+
+            let body = { //產品名稱
+                "firstDay": this.startDate,
+                "endDay": this.endDate
+            }
+
+            fetch("http://localhost:8080/search_period", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            })
+                .then(response => {
+                    //vue不能用fetch+function 要用箭頭
+                    //從JSON格式轉回Js物件
+                    return response.json()
+                })
+                .then(data => {
+
+
+
+                    console.log(data);
+                    this.searchResultArr = data.product_list;
+                    alert(data.message)
+
+
+
+
+                })
+
+
+
+
+
+
+
+        },
+        getShoppingCar() {//獲取購物車資料
+            let body = {
+                "buyerAccount": localStorage.getItem('email') //需要localstorage資料
+            }
+            fetch("http://localhost:8080/get_shopping_data", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body)
+            })
+                .then(response => {
+                    //vue不能用fetch+function 要用箭頭
+                    //從JSON格式轉回Js物件
+                    return response.json()
+                })
+                .then(data => {
+
+                    let priceArr = [];
+                    let SearchArr = [];
+                    console.log(data);
+                    // alert(data.msg);
+                    SearchArr = data.Shopping_Detail_List;
+                    for (let i = 0; i < SearchArr.length; i++) {
+                        let singleTotal;
+                        let singalPrice = data.Shopping_Detail_List[i].itemPrice;
+                        let singleNum = data.Shopping_Detail_List[i].itemNum;
+                        singleTotal = singalPrice * singleNum
+                        priceArr.push(singleTotal);
+                    }
+
+                    console.log(priceArr);
+                    let totoalCollection = 0;
+                    for (let i = 0; i < priceArr.length; i++) {
+                        totoalCollection += priceArr[i]
+
+                    }
+                    console.log(totoalCollection);
+                    this.totalPrice = totoalCollection;
+                    // console.log(SearchArr);
+                    this.chartResultArr = SearchArr; //要呈現的清單表不能刪
+                    // console.log(this.chartResultArr);
+                })
+        },
+        getemit() {  //加入購物車要刷新右邊購物車  
+            this.getShoppingCar();
+        },
+        getSelectemit() {//更新購物車也emit指令
+            console.log(`ASD`)
+            this.getShoppingCar();
+        },
+
+
+
+
+
+
+
+    }, watch: {
+        startDate(newStartDate) {
+            const maxAllowedDate = new Date();
+            maxAllowedDate.setDate(maxAllowedDate.getDate() - 30); // 取得 30 天前的日期
+
+            if (new Date(newStartDate) < maxAllowedDate) {
+                this.isStartDateInvalid = true;
+            } else {
+                this.isStartDateInvalid = false;
+            }
+        }
     },
+
+
+
+
     // 生命週期}
     mounted() {
-        
-       
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0');
+        const day = String(today.getDate()).padStart(2, '0');
+        this.startDate = `${year}-${month}-${day}`;
+        this.endDate = `${year}-${month}-${day}`;
 
+        this.getShoppingCar();
     }
 }
-
 
 
 
@@ -66,103 +241,253 @@ export default {
 <template>
     <div class="search-area">
         <div class="searchbar">
-       
-            <label for="searchBar">商品</label>
-            <input class="searchBar" type="text" name="" id="searchBar" placeholder="關鍵字搜尋"
-                v-model="this.searchData">
-            <button type="button" class="searchReqBtn" @click="getProductInfo">
-                <i class="fa-solid fa-magnifying-glass">搜尋</i>
-            </button>
-            <button type="button" class="searchReqBtn" @click="">
-                <i class="fa-solid fa-pen-to-square">購物車</i>
-                
-            </button>
-        </div>
+            <div class="label">
 
+                <div class="searchIcon">
+                    <label for="searchBar"></label>
+                </div>
+                <!-- =============================================== -->
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ searchTypeBtn }}
+
+
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                        <li><button class="dropdown-item" type="button" v-on:click="changeAndSearchPlace">{{ searchTypePlace
+                        }}</button></li>
+                        <li><button class="dropdown-item" type="button" v-on:click="changeAndSearchNmae">{{
+                            searchTypeProduct }}</button></li>
+                        <!-- <li><button class="dropdown-item" type="button">Something else here</button></li> -->
+                    </ul>
+                </div>
+                <!-- =========================================================== -->
+                <select name="" id="tool" v-on:change="changeAndSearchPlace">
+                    <option value="嘉義縣">嘉義縣</option>
+                    <option value="新北市">新北市</option>
+                    <option value="嘉義市">嘉義市</option>
+                    <option value="新竹縣">新竹縣</option>
+                    <option value="新竹市">新竹市</option>
+                    <option value="臺北市">臺北市</option>
+                    <option value="臺南市">臺南市</option>
+                    <option value="宜蘭縣">宜蘭縣</option>
+                    <option value="苗栗縣">苗栗縣</option>
+                    <option value="雲林縣">雲林縣</option>
+                    <option value="花蓮縣">花蓮縣</option>
+                    <option value="臺中市">臺中市</option>
+                    <option value="臺東縣">臺東縣</option>
+                    <option value="桃園市">桃園市</option>
+                    <option value="南投縣">南投縣</option>
+                    <option value="高雄市">高雄市</option>
+                    <option value="金門縣">金門縣</option>
+                    <option value="屏東縣">屏東縣</option>
+                    <option value="基隆市">基隆市</option>
+                    <option value="澎湖縣">澎湖縣</option>
+                    <option value="彰化縣">彰化縣</option>
+                    <option value="連江縣">連江縣</option>
+                </select>
+
+                <!-- ============================================================================ -->
+                <input class="searchBar" type="text" name="" id="searchBar" placeholder="產品名稱關鍵字搜尋"
+                    v-model="this.searchData">
+                <button type="button" class="searchReqBtn" @click="getProductInfo">
+                    搜尋
+                </button>
+            </div>
+
+            <!-- 日期範圍搜尋 -->
+            <div class="datesearch">
+
+                <label for="dateFrom">產品日期範圍(起)</label>
+                <input type="date" class="form-control" id="dateFrom" name="date" v-model="startDate">
+                <label for="dateFrom">產品日期範圍(訖)</label>
+                <input type="date" class="form-control" id="dateUtil" name="date" v-model="endDate">
+                <span v-if="endDate < startDate" class="error-message">起訖日期順序"不合理"</span>
+                <span v-else-if="isStartDateInvalid" class="error-message">查詢30天前生鮮食品為"不合理"</span>
+                <button class="dateSearchBtn" @click="getDateInfo"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+
+
+
+
+
+        </div>
 
 
     </div>
 
-    <div class="background-layout">
-      
-        <layoutCardView v-for="(item, index) in searchResultArr" 
-            v-bind:searchResultcode="item.hsCode"
-            v-bind:selleraccount="item.sellerAccount" 
-            v-bind:date="item.date" 
-            v-bind:description="item.description"
-            v-bind:name="item.name" v-bind:number="item.number" v-bind:place="item.place" v-bind:price="item.price"
-            v-bind:type="item.type" 
 
-           
-         
-              />
-              <!-- @click="setShoppingList()" -->  
-            
-          
-        
+
+
+    <div class="content-layout">
+        <div class="background-layout">
+
+            <layoutCardView v-for="(item, index) in searchResultArr" v-bind:searchResultcode="item.hsCode"
+                v-bind:selleraccount="item.sellerAccount" v-bind:date="item.date" v-bind:description="item.description"
+                v-bind:name="item.name" v-bind:number="item.number" v-bind:place="item.place" v-bind:price="item.price"
+                v-bind:type="item.type" v-on:emitPushAdd="getemit" />
+            <!-- @click="setShoppingList()" -->
+        </div>
+        <div class="right-shoppingCart">
+
+            <div class="upper-dtail">
+                <h5 class="buyerTitle">買家已選清單</h5>
+                <selectionView v-for="(item, index) in chartResultArr" v-bind:selectShopppingCode="item.shoppingNumber"
+                    v-bind:selectId="item.itemId" v-bind:selectName="item.itemName" v-bind:selectNumber="item.itemNum"
+                    v-bind:selectPrice="item.itemPrice" v-bind:selectStock="item.stock" v-on:emitPushModi="getSelectemit" />
+            </div>
+            <div class="bottom-total">
+                <div class="price-total">
+                    <h4>購物車總金額:</h4>
+                    <h4 class="text-danger">{{ this.totalPrice }}</h4>
+                </div>
+                <div >
+                    <button class="checkbtn" type="button">前往結帳</button>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
+.buyerTitle{
+    text-align: center;
+}
+.content-layout {
+    display: flex
+}
+
+.right-shoppingCart {
+    flex-direction: column;
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+
+    .upper-dtail {
+        width: 35vw;
+        height: 45vh;
+        background: #ffffff;
+        border-radius: 5px;
+        overflow-y: auto;
+    }
+
+    .bottom-total {
+        width: 35vw;
+        height: 30vh;
+        background: #ffffff;
+        border-radius: 5px;
+        margin-top: 30px;
+        flex-direction: column;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+}
+
 .background-layout {
     flex-wrap: wrap;
+    flex-direction: column;
     display: flex;
     justify-content: left;
-    align-items: center;
+    align-items: start;
+
     border-radius: 5px;
-    width: 65vw;
+    width: 37vw;
     height: 80vh;
     background: #ffffff;
     overflow-y: auto;
+    margin-right: 3vw;
 
 
 }
 
+select {
+    height: 2vw;
+    border-radius: 5px;
+}
 
 .search-area {
 
     margin-bottom: 10px;
 }
 
+#tool {
+    margin: 10px;
+}
+
 .searchbar {
-    width: 65vw;
+    width: 75vw;
     height: 3.5vw;
     background-color: rgb(255, 255, 255);
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     font-size: 18px;
-    border-radius: 20px;
+    border-radius: 30px;
     border: 2px solide rgb(45, 50, 46);
-    
-    .searchBar{
-        outline:none;
+    padding: 15px;
+
+    .searchBar {
+        border-radius: 30px;
+        border: 2px solid rgb(67, 90, 72);
+        outline: none;
+
     }
 
 }
 
+.label {
+    width: 50%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.datesearch {
+    width: 50%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.searchIcon {
+    margin: 5px;
+    width: 2vw;
+    height: 2vw;
+    border-radius: 50px;
+    border: 2px solid rgb(67, 90, 72);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: rgb(67, 90, 72);
+}
+
 label {
+
     width: 100px;
     padding: 15px;
-    color:rgb(88, 161, 62);
-    border-radius: 16px;
+    color: rgb(88, 161, 62);
+
     font-weight: bold;
-    font-size: 24px;
+    font-size: 16px;
     // margin-right: 30px;
 }
 
 input {
     text-align: center;
     border: none;
-    width: 50vw;
+    width: 20vw;
     height: 2vw;
     // margin-right: 50px;
 }
 
 .searchReqBtn {
     background-color: rgb(67, 90, 72);
-    border-radius: 16px;
+    border-radius: 25px;
     color: rgb(223, 226, 224);
     transition: 0.6s;
     width: 120px;
@@ -178,5 +503,55 @@ input {
 
 .searchReqBtn:active {
     scale: 0.9;
+}
+
+.form-control {
+    width: 8vw;
+    border: 2px solid rgb(67, 90, 72);
+}
+
+.error-message {
+    color: red;
+    font-size: 16px;
+}
+
+.dateSearchBtn {
+    width: 2vw;
+    height: 2vw;
+    border-radius: 50%;
+    border: 2px solid rgb(229, 235, 230);
+    background: rgb(67, 90, 72);
+    color: antiquewhite;
+    transition: 1s;
+
+}
+
+.dateSearchBtn:hover {
+    background: rgb(176, 185, 178);
+    color: rgb(48, 47, 46);
+}
+
+.dateSearchBtn:active {
+    scale: 0.6;
+}
+.price-total{
+    flex-direction: column;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.checkbtn{
+    background: #273229;
+    color: #ffffff;
+    border-radius: 5px;
+    transition: 0.6s;
+
+}
+.checkbtn:hover{
+    background: #ffffff;
+    color: #273229;
+}
+.checkbtn:active{
+   scale: 0.7;
 }
 </style>
